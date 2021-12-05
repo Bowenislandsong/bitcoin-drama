b1c36a01cbf594e62ed3e8f22c353fd1259ded99
bowensong <bowenson@usc.edu>
2021-12-04 16:17:34 -0800

    [py] update webcrawler and added data for the atm depoisit and siwthdraw history address

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # bitcoin-drama
+
+# Webcrawling
+When crawling against blockchain.com, to avoid rate limit, use `tor` and `proxychains` combination to crawl with different ip each time. 
--- webcrawler/webgetter.py
+++ webcrawler/webgetter.py
@@ -1,6 +1,5 @@
-from dataclasses import dataclass
 from typing import List
-import datetime,sys
+import sys
 import requests
 from bs4 import BeautifulSoup as bs
 import json,time,random
@@ -9,26 +8,6 @@ import json,time,random
 txurl = "https://blockchain.info/rawaddr/"
 balurl = "https://blockchain.info/balance?active="
 offset = "&offset="
-
-# addrMode = "address/"
-# transMode = "tx/"
-
-@dataclass
-class BTCTransaction:
-    hash: str           # transaction hash
-    fee: float          # transaction gas fee
-    sender: dict()      # Sender address and value
-    receiver: dict()    # receiver address and value
-    txTime: datetime.datetime # Transaction time
-    TotalTxValue: float # total transaction Value
-
-@dataclass
-class BTCAddress:
-    hash: str
-    numTx: int
-    TotRx: float
-    TotTx: float
-    FBalance: float
 
 
 class webgetter():
@@ -43,9 +22,7 @@ class webgetter():
     def getTxs(self,numTx:int=sys.maxsize):
         numTx = min(self.ntx,numTx)
         for i in range((numTx//50)+1):
-            # print(txurl+self.haddress+"?"+offset+str(i*50)+"&limit="+str(50 if numTx>50 else numTx))
             tx = self.__getweb(txurl+self.haddress+"?"+offset+str(i*50)+"&limit="+str(50 if numTx>50 else numTx))
-            # print(tx)
             if i ==0:
                 self.data = tx
             else:
@@ -62,11 +39,10 @@ class webgetter():
 
     def __getweb(self,url):
         getweb = lambda : bs(requests.get(url).text,'html.parser')
-        # print(url+self.mode+self.haddress+"?page="+str(page))
         return json.loads(str(getweb()))
 
 if __name__ == '__main__':
-    sampleaddr = "bc1qsm9m9nuhpr8sg0tzrhpkkzsl6nvn5gf4ddjvf8nymc9lufjde6lqu6rxvj"
+    sampleaddr = "31w8Lc9V34ZYyYMycPcQjjkuBWLn8oYmxd"
     w = webgetter(sampleaddr)
-    w.getTxs(6)
-    w.saveToFile("temp.json")
+    w.getTxs()
+    w.saveToFile("widthdraw.json")
